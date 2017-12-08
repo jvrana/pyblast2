20352f9eff8916a715dd18322e59eadb5f74486b
jvrana <justin.vrana@gmail.com>
2017-12-08 13:40:43 -0800

    fix test utils

--- tests/test_seqio.py
+++ tests/test_seqio.py
@@ -2,19 +2,27 @@ import pytest
 import os
 from pyblast import *
 
+
+@pytest.fixture(scope="module")
+def get_path(here):
+    def wrapped(name):
+        return os.path.join(here, 'data/test_data/seqio_plasmids', name)
+    return wrapped
+
+
 def test_circular():
-    directory = os.path.abspath("tests/data/test_data/seqio_plasmids")
-    assert dna_at_path_is_circular(os.path.join(directory, "circular_test_dna.gb")) == True
-    assert dna_at_path_is_circular(os.path.join(directory, "linear_test_dna.gb")) == True
+    assert get_path("circular_test_dna.gb")
+    assert get_path("linear_test_dna.gb")
 
-def test_open_sequence():
-    directory = os.path.abspath("tests/data/test_data/seqio_plasmids")
+
+def test_open_sequence(get_path):
     with pytest.raises(ValueError):
-        open_sequence(os.path.join(directory, "alsdkjfaldsf"))
-    print(open_sequence(os.path.join(directory, "linear_test_dna.gb")))
+        open_sequence(get_path("alsdkjfaldsf"))
+    print(open_sequence(get_path("linear_test_dna.gb")))
 
-def test_sanitize_filename():
-    directory = os.path.abspath("tests/data/test_data/seqio_plasmids/to_be_sanitized")
+
+def test_sanitize_filename(here):
+    directory = os.path.join(here, 'data/test_data/seqio_plasmids/to_be_sanitized')
     sanitize_filenames(directory)
     for f in os.listdir(directory):
         assert not ' ' in f
@@ -22,7 +30,8 @@ def test_sanitize_filename():
     for f in os.listdir(directory):
         assert not '_' in f
 
-def test_concat_seqs():
-    directory = os.path.abspath("tests/data/test_data/templates")
-    out = os.path.abspath("tests/data/test_data/seqio_plasmids/concatenated/concat.fasta")
+
+def test_concat_seqs(here):
+    directory = os.path.join(here, "data/test_data/templates")
+    out = os.path.join(here, "data/test_data/seqio_plasmids/concatenated/concat.fasta")
     concat_seqs(directory, out, savemeta=True)
