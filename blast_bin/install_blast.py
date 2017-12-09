1e1769e2bc3745c31e6d646b6dffbcb0b97f22f2
jvrana <justin.vrana@gmail.com>
2017-12-09 13:17:40 -0800

    hotfix, better help message

--- blast_bin/install_blast.py
+++ blast_bin/install_blast.py
@@ -190,7 +190,8 @@ def install_blast_using_ftp(config):
 def main():
     parser = argparse.ArgumentParser(description="Install BLAST from ncbi")
     parser.add_argument("user_email", type=str, help="A user email is required for BLAST download from ncbi.")
-    parser.add_argument("platform", type=str, help="Choose your platform. Choose from {}".format(get_formats().keys()))
+    parser.add_argument("platform", type=str, help="Choose your platform. Choose from {}".format(
+        ', '.join(["'{}'".format(x) for x in get_formats().keys()])))
     parser.add_argument("--f", help="Forces install")
     args = parser.parse_args()
     force = False
