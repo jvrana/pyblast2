3aabc64150d2658580588d55aac287c246f42b16
jvrana <justin.vrana@gmail.com>
2019-07-03 17:27:06 -0700

    Update test_bioblast.py

--- tests/test_blast/test_bioblast.py
+++ tests/test_blast/test_bioblast.py
@@ -3,38 +3,47 @@ from Bio.SeqRecord import SeqRecord
 from pyblast import BioBlast
 
 
-def compute_length(s, e, l, circular):
-    length = e - s
-    if length < 0 and not circular:
-        raise Exception("not circular")
-    elif length < 0 and circular:
-        length = length + l + 1
-    return length
+class Validator(object):
+    @staticmethod
+    def compute_length(s, e, l, circular):
+        length = e - s
+        if length < 0 and not circular:
+            raise Exception("not circular")
+        elif length < 0 and circular:
+            length = length + l + 1
+        return length
 
+    @classmethod
+    def location_length(cls, record):
+        return cls.compute_length(
+            record.features[0].location.start,
+            record.features[0].location.end,
+            len(record),
+            record.annotations["circular"],
+        )
 
-def location_length(record):
-    return compute_length(
-        record.features[0].location.start,
-        record.features[0].location.end,
-        len(record),
-        record.annotations["circular"],
-    )
+    @classmethod
+    def region_length(cls, region):
+        return cls.compute_length(
+            region["start"], region["end"], region["length"], region["circular"]
+        )
 
-
-def region_length(region):
-    return compute_length(
-        region["start"], region["end"], region["length"], region["circular"]
-    )
-
-
-def validate_results(results):
-    for r in results:
-        assert region_length(r["query"]) == region_length(r["subject"])
-
-
-def validate_blaster_results(blaster):
-    for r, alignment in zip(blaster.results, blaster.alignments()):
-        assert location_length(alignment[0]) == location_length(alignment[1])
+    @classmethod
+    def validate_blaster_results(cls, blaster):
+        errors = []
+        for r, alignment in zip(blaster.results, blaster.alignments()):
+            if not cls.location_length(alignment[0]) == cls.location_length(
+                alignment[1]
+            ):
+                errors.append(
+                    (
+                        "Alignments have different lengths: {}".format(alignment),
+                        alignment,
+                    )
+                )
+            if not cls.region_length(r["query"]) == cls.region_length(r["subject"]):
+                errors.append(("Regions have different lengths: {}".format(r), r))
+        assert not errors
 
 
 def test_basic_run():
@@ -53,7 +62,7 @@ def test_basic_run():
     print(alignments)
 
 
-def test_main(new_bio_blast):
+def test_valid_results(new_bio_blast):
     blast = new_bio_blast()
     blast.quick_blastn()
-    validate_blaster_results(blast)
+    Validator.validate_blaster_results(blast)
