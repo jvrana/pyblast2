4a530cefbf701a50719d688d45216d88f14bc0cd
Justin Vrana <justin.vrana@gmail.com>
2019-07-05 11:13:29 -0700

    added keats

---
+++ .keats/changelog.md
@@ -0,0 +1,8 @@
+# pyblast change log
+## 1.0.0a3
+
+**2019-07-05T11:12:30.982472**
+minor change to installation
+
+ - added keats pkg management
+ - verison maintained in __version__.py
--- pyblast/__init__.py
+++ pyblast/__init__.py
@@ -1,4 +1,4 @@
 """PyBlast"""
 
 from .blast import BlastBase, BioBlast, JSONBlast, TmpBlast
-from ._version import __version__, __title__, __author__, __homepage__, __repo__
+from .__version__ import __version__, __title__, __authors__, __homepage__, __repo__
