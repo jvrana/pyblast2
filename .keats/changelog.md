d5d11e65ca34a295efd4c48f6fd074842ac1eb34
Justin Vrana <justin.vrana@gmail.com>
2019-07-05 11:14:41 -0700

    release - formatting for release 1.0.0a3

--- .keats/changelog.md
+++ .keats/changelog.md
@@ -1,8 +1,8 @@
 # pyblast change log
 ## 1.0.0a3
 
-**2019-07-05T11:12:30.982472**
-minor change to installation
+**2019-07-05T11:14:35.956014**
+updated package management and version
 
  - added keats pkg management
  - verison maintained in __version__.py
--- pyblast/__version__.py
+++ pyblast/__version__.py
@@ -3,7 +3,7 @@
 __version__ = "1.0.0a3"
 __name__ = "pyblastbio"
 __title__ = "pyblastbio"
-__authors__ = ['First Last <youemail@domain.com>']
+__authors__ = ["First Last <youemail@domain.com>"]
 __repo__ = None
 __homepage__ = "https://www.github.com/jvrana/pyblast"
 __description__ = ""
